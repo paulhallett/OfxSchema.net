c1c8f5f66a85e7a2625669c1b05c807bd4adf62e
Paul Hallett <paul.hallett@live.com>
2025-08-26 20:27:36 +1200

    Add custom XmlConverters for string and integer

---
+++ src/Extensions/XmlConverters.cs
@@ -0,0 +1,53 @@
+namespace OfxSchema;
+
+public abstract class XmlConverter<T>
+{
+    public abstract T? Read(System.Xml.XmlReader reader, Type typeToConvert);
+    public abstract void Write(System.Xml.XmlWriter writer, T value);
+}
+
+public class StringTypeXmlConverter : XmlConverter<string>
+{
+    public override string? Read(System.Xml.XmlReader reader, Type typeToConvert)
+    {
+        if (reader.IsEmptyElement)
+        {
+            reader.ReadStartElement();
+            return null;
+        }
+        
+        reader.ReadStartElement();
+        var value = reader.ReadContentAsString();
+        reader.ReadEndElement();
+        
+        return string.IsNullOrEmpty(value) ? null : value;
+    }
+
+    public override void Write(System.Xml.XmlWriter writer, string value)
+    {
+        writer.WriteString(value);
+    }
+}
+
+public class IntegerTypeXmlConverter : XmlConverter<int>
+{
+    public override int Read(System.Xml.XmlReader reader, Type typeToConvert)
+    {
+        if (reader.IsEmptyElement)
+        {
+            reader.ReadStartElement();
+            return 0;
+        }
+        
+        reader.ReadStartElement();
+        var value = reader.ReadContentAsInt();
+        reader.ReadEndElement();
+        
+        return value;
+    }
+
+    public override void Write(System.Xml.XmlWriter writer, int value)
+    {
+        writer.WriteValue(value);
+    }
+}
---
+++ src/Usings.cs
@@ -0,0 +1,7 @@
+global using System;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
+global using System.Text;
+global using System.Xml;
+global using System.Xml.Schema;
+global using System.Xml.Serialization;
